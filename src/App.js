14c6e22f0293cd0d69a04353204a208344ab77e9
Juan Teles Dias <70079513+JuanTeles@users.noreply.github.com>
2025-11-22 18:28:07 -0300

    fix: remove Router duplicado e corrige Home como rota inicial

--- src/App.js
+++ src/App.js
@@ -17,35 +17,34 @@ const App = () => {
     return (
         // Esta <div> simula as classes do body (d-flex flex-column min-vh-100)
         <div className="d-flex flex-column min-vh-100"> 
-            <Router>
-                <Header />
-                
-                {/* O <main> com flex-grow-1 garante que o conteúdo das Rotas ocupe o espaço restante */}
-                <main className="flex-grow-1 d-flex align-items-center justify-content-center">
-                    <Routes>
-                        {/* 1. Rota para a Home Page */}
-                        <Route path="/" element={<Home />} />
+            <Header />
+            
+            {/* O <main> com flex-grow-1 garante que o conteúdo das Rotas ocupe o espaço restante */}
+            <main className="flex-grow-1 d-flex align-items-center justify-content-center">
+                <Routes>
+                    {/* 1. Rota para a Home Page */}
+                    <Route path="/" element={<Home />} />
 
-                        {/* 2. Rota para a Página Sobre */}
-                        <Route path="/about" element={<About />} />
+                    {/* 2. Rota para a Página Sobre */}
+                    <Route path="/about" element={<About />} />
 
-                        {/* 3. Rota para a Página de Cadastro */}
-                        <Route path="/cadastro" element={<Cadastro />} />
-                        
-                        {/* 4. Rota para a Página de Cadastro */}
-                        <Route path="/login" element={<Login />} />
+                    {/* 3. Rota para a Página de Cadastro */}
+                    <Route path="/cadastro" element={<Cadastro />} />
+                    
+                    {/* 4. Rota para a Página de Cadastro */}
+                    <Route path="/login" element={<Login />} />
 
-                        {/* 5. Rota para a Página de Painel Administrativo */}
-                        <Route path="/paineladm" element={<Painel />} />
+                    {/* 5. Rota para a Página de Painel Administrativo */}
+                    <Route path="/paineladm" element={<Painel />} />
 
-                        {/* Rotas futuras a serem criadas */}
-                        <Route path="/fluxo" element={<h1>Página de Fluxo de Caixa</h1>} />
-                        <Route path="/mensalidades" element={<h1>Página de Mensalidades</h1>} />
-                    </Routes>
-                </main>
+                    {/* Rotas futuras a serem criadas */}
+                    <Route path="/fluxo" element={<h1>Página de Fluxo de Caixa</h1>} />
+                    <Route path="/mensalidades" element={<h1>Página de Mensalidades</h1>} />
+                </Routes>
+            </main>
 
-                <Footer />
-            </Router>
+            <Footer />
+        
         </div>
     );
 };

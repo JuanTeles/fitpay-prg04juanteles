8a5101b69c5d2fb69dc63d667dd7c1313433f04f
Juan Teles Dias <70079513+JuanTeles@users.noreply.github.com>
2025-12-18 18:11:15 -0300

    ✨ feat(api): configura URL base via variáveis de ambiente

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080', // Porta onde o Spring está rodando
+    baseURL: process.env.REACT_APP_API_BASE_URL, // Porta onde o Spring está rodando
     headers: {
         'Content-Type': 'application/json'
     }
